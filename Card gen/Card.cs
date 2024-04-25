cbaf24ff2a9febee560a02c72613c9d366705422
WhyCee <97743586+WhyC33@users.noreply.github.com>
2024-04-25 12:26:52 +0200

    Title screen/ kode
    Title screen/ kode

--- Card gen/Card.cs
+++ Card gen/Card.cs
@@ -2,10 +2,52 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public partial class Card : Node2D
+public partial class CardSystem : Node2D
 {
-	public int Suit { get; }
-    public int Rank { get; }
+    List<card> cards = new List<card>();
+    public class card
+    {
+        public int suit { get; set; } = 0; //0 = diamonds 1 = clubs 2 = hearts 3 = spades
+        public int rank { get; set; } = 0; //1-13
+        public bool faceup { get; set; } = false; //boolean for if the card is face up or not
+
+        public card(int suit, int rank)
+        {
+            this.suit = suit;
+            this.rank = rank;
+            this.faceup = faceup;
+        }    
+    }
+
+public void Deck(){
+    createDeck();
+    ShuffleDeck();
+}
+public void createDeck(){
+    GD.Print("Creating deck");
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 1; j <= 13; j++)
+        {
+            card card = new card(i, j); // Create a new card object
+            cards.Add(card); // Add the card to the list
+        }
+    }
+}
+public void ShuffleDeck(){
+    //randomize the deck
+    GD.Print("Shuffling deck");
+    Random rnd = new Random();
+    for (int i = 0; i < cards.Count; i++)
+    {
+        int r = rnd.Next(cards.Count);
+        card temp = cards[i];
+        cards[i] = cards[r];
+        cards[r] = temp;
+    }
+}
+
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -16,10 +58,4 @@ public partial class Card : Node2D
 	public override void _Process(double delta)
 	{
 	}
-    
-	public Card(int suit, int rank)
-    {
-        Suit = suit; // Using numbers for suits (0-3)
-        Rank = rank; // Using numbers for ranks (1-13)
-    }
 }
--- Controller/Mainscene.cs
+++ Controller/Mainscene.cs
@@ -5,37 +5,32 @@ using System.Collections.Generic;
 
 public partial class Mainscene : Node2D
 {
+    public Label _TidTager;
+	public Timer _Timer;
+	public int TimeStarted = 0;
+
 
 	static List<Card> cards = new List<Card>();
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_TidTager = GetNode<Label>("GUI/Timer/TidTager");
+		_Timer = GetNode<Timer>("GUI/Timer");
+		_Timer.Start();
 	}
+
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
-/* skal lige fixes
-public void Card_gen(){
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 1; j <= 13; j++)
-            {
-                Card card = new Card(i, j); // Create a new card object
-                cards.Add(card); // Add the card to the list
-            }
-        }
-
-        // Display the generated cards in the console
-        foreach (Card card in cards)
-        {
-            Console.WriteLine($"Card: {card.Rank} of {card.Suit}");
-        }
-     }
+public void _DisplayTimer(){
+	TimeStarted++;
+	_TidTager.Text = "Tid: " + TimeStarted;
 }
-*/
+
+
 public void Stack_Check(){ //
 
  }

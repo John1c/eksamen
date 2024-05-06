a07f6200bad37b875063b12d7c40671d14b884d2
John1c <nichlaskaasleth@gmail.com>
2024-05-06 13:03:07 +0200

    List and shuffle
    Added list for cards
    Able to shuffle the cards as well. Not visually

--- Card gen/Deck.cs
+++ Card gen/Deck.cs
@@ -6,19 +6,19 @@ public partial class Deck : Node2D
 {
 	
 	List<card> cards = new List<card>();
-    public class card
-    {
-        public int suit { get; set; } = 0; //0 = diamonds 1 = clubs 2 = hearts 3 = spades
-        public int rank { get; set; } = 0; //1-13
-        public bool faceup { get; set; } = false; //boolean for if the card is face up or not
+	public class card
+	{
+		public int suit { get; set; } = 0; //0 = diamonds 1 = clubs 2 = hearts 3 = spades
+		public int rank { get; set; } = 0; //1-13
+		public bool faceup { get; set; } = false; //boolean for if the card is face up or not
 
-        public card(int suit, int rank)
-        {
-            this.suit = suit;
-            this.rank = rank;
-            this.faceup = faceup;
-        }    
-    }
+		public card(int suit, int rank)
+		{
+			this.suit = suit;
+			this.rank = rank;
+			this.faceup = faceup;
+		}    
+	}
 
 public void StartDeck(){
 	createDeck();
@@ -26,27 +26,27 @@ public void StartDeck(){
 }
 
 public void createDeck(){
-    GD.Print("Creating deck");
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 1; j <= 13; j++)
-        {
-            card card = new card(i, j); // Create a new card object
-            cards.Add(card); // Add the card to the list
-        }
-    }
+	GD.Print("Creating deck");
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 1; j <= 13; j++)
+		{
+			card card = new card(i, j); // Create a new card object
+			cards.Add(card); // Add the card to the list
+		}
+	}
 }
 public void ShuffleDeck(){
-    //randomize the deck
-    GD.Print("Shuffling deck");
-    Random rnd = new Random();
-    for (int i = 0; i < cards.Count; i++)
-    {
-        int r = rnd.Next(cards.Count);
-        card temp = cards[i];
-        cards[i] = cards[r];
-        cards[r] = temp;
-    }
+	//randomize the deck
+	GD.Print("Shuffling deck");
+	Random rnd = new Random();
+	for (int i = 0; i < cards.Count; i++)
+	{
+		int r = rnd.Next(cards.Count);
+		card temp = cards[i];
+		cards[i] = cards[r];
+		cards[r] = temp;
+	}
 }
 
 
@@ -54,11 +54,11 @@ public void ShuffleDeck(){
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        StartDeck();
-        foreach (card card in cards)
-        {
-            GD.Print(card.rank + " of " + card.suit);
-        }
+		StartDeck();
+		foreach (card card in cards)
+		{
+			GD.Print(card.rank + " of " + card.suit);
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- Card/Cardcontroller.cs
+++ Card/Cardcontroller.cs
@@ -1,16 +1,14 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-using System.Numerics;
 
 public partial class Cardcontroller : Node2D
 {
 		private bool isDragging = false;
 	    private int _cardID;
         private int _CardPattern;
+		private bool _faceUp = false;
 	    private Label _CardLabel;
-		
-		public bool IsStacked = false;
 
     public int cardID {
 		get{ return _cardID; }
@@ -22,6 +20,11 @@ public partial class Cardcontroller : Node2D
 		set{ _CardPattern = value; }
 	}
 
+   public bool faceUp {
+		get{ return _faceUp; }
+		set{ _faceUp = value; }
+	}
+
 public void OnCardInstantiate(int ID, int Pattern){
         cardID = ID;
         CardPattern = Pattern;
@@ -29,7 +32,7 @@ public void OnCardInstantiate(int ID, int Pattern){
         _CardLabel.Text = "ID: "+ ID.ToString() +"\n"+ "Pattern: " + Pattern.ToString();
     }
 
-	private Godot.Vector2 offset = new(0,0);
+	private Vector2 offset = new(0,0);
     public override void _Input(InputEvent @event)
 	{
 	// Mouse in viewport coordinates.
@@ -42,40 +45,17 @@ public void OnCardInstantiate(int ID, int Pattern){
 		} 
 	}
 
-    private void MoveWithMouse(Godot.Vector2 v)
+    private void MoveWithMouse(Vector2 v)
     {
         Position = v;
-		IsStacked = false;
     }
     
-	public void move_to_front(){
-		GetParent().MoveChild(this, GetParent().GetChildCount());
-	}
-
     public void OnMouseDown() {
 		isDragging = true;
-		move_to_front();
 	}
 
 	public void OnMouseUp(){
 		isDragging = false;
-	}
-	public void _on_area_2d_area_entered(Area2D area){
-		GD.Print("Area Entered");
-		Stack_on_card(area);
-	}
-	public void Stack_on_card(Area2D area){
-		Cardcontroller card = area.GetParent() as Cardcontroller;
-
-		if(isDragging && (cardID+1 == card.cardID && (CardPattern-1 == card.CardPattern || CardPattern+1 == card.CardPattern))){
-		//move the card to the top of the stack
-		move_to_front();
-		//move the card to the position of the card it is stacked on
-		isDragging = false;
-		IsStacked = true;
-        Position = card.Position + new Godot.Vector2(0, 60);
-		//Movecount++; senere implementering
-		}
 	}
 
 	// Called when the node enters the scene tree for the first time.
@@ -87,11 +67,5 @@ public void OnCardInstantiate(int ID, int Pattern){
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		/* virker ikke endnu skal lige fået lortet til at com på hindanden
-		if(IsStacked){
-			//Move the card to the position of the card it is stacked on
-			Position = card.Position + new Godot.Vector2(0, 60);
-		}
-		*/
 	}
 }

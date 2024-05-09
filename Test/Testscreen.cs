06c46df5f3c164785b345eedbc028e0b62d593a0
John1c <nichlaskaasleth@gmail.com>
2024-05-09 23:24:21 +0200

    last fixes

--- Test/Testscreen.cs
+++ Test/Testscreen.cs
@@ -16,7 +16,6 @@ public partial class Testscreen : Node2D
 		cardPrefab = ResourceLoader.Load("uid://cfufscq00nrbm") as PackedScene;
 		InstantiateCard();
 		PileMaker();
-		//draw_card();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -65,7 +64,15 @@ public partial class Testscreen : Node2D
 			cards[i].UpdateCard();
 		}
 	}
+<<<<<<< Updated upstream
 	public void ShuffleDeck()
+=======
+}
+
+public void PileMaker()
+{
+	for(int i = 0; i < 7; i++)
+>>>>>>> Stashed changes
 	{
 		//randomize the deck
 		GD.Print("Shuffling deck");
@@ -77,6 +84,7 @@ public partial class Testscreen : Node2D
 			cards[i] = cards[r];
 			cards[r] = temp;
 		}
+<<<<<<< Updated upstream
 	}
 	public void PileMaker()
 	{
@@ -116,6 +124,26 @@ public partial class Testscreen : Node2D
 	public void update_pile()
 	{
 		for (int i = 0; i < piles.Count; i++)
+=======
+	tempPile.Reverse();
+	tempPile[0].faceUp = true;
+	tempPile[0].UpdateCard();
+	bool occupyState = true;
+	states.Add(occupyState);
+	GD.Print(states[i]);
+
+	
+	piles.Add(tempPile);
+	GD.Print("Cap: " + piles[i].Capacity.ToString());
+	}
+	
+	}
+	public void update_pile()
+	{
+	for(int i = 0; i < piles.Count; i++)
+	{ 
+	if(piles[i].Count == 0 /*&& states[i] == false*/)
+>>>>>>> Stashed changes
 		{
 			if (piles[i].Count == 0)
 			{
